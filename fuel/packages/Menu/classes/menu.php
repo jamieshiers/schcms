d424fbed6c3854df8023ceddab7963a6b45f3c48
jamieshiers <jamie@digitalschool.co.uk>
2012-09-16 17:51:22 +0100

    Updates for pagodabox

--- fuel/packages/Menu/classes/menu.php
+++ fuel/packages/Menu/classes/menu.php
@@ -12,7 +12,7 @@ class MenuBuilder
 {
 	var $items = array();
 	var $html  = array();
-	
+
 	/**
 	 * Build the HTML for the menu 
 	 */
@@ -20,26 +20,26 @@ class MenuBuilder
 	{
 		$html  = array();
 		$items = \DB::select()->from('menus')->where('active', 1)->order_by('parent_id', 'asc')->order_by('position', 'asc')->as_assoc()->execute();
-		
+
 		foreach ( $items as $item )
 			$children[$item['parent_id']][] = $item;
-		
+
 		// loop will be false if the root has no children (i.e., an empty menu!)
 		$loop = !empty( $children[$root_id] );
-		
+
 		// initializing $parent as the root
 		$parent = $root_id;
 		$parent_stack = array();
-		
+
 		// HTML wrapper for the menu (open)
 		$html[] = "<ul>";
-		
+
 		while ( $loop && ( ( $option = each( $children[$parent] ) ) || ( $parent > $root_id ) ) )
 		{
 			if ( $option === false )
 			{
 				$parent = array_pop( $parent_stack );
-				
+
 				// HTML for menu item containing childrens (close)
 				$html[] = str_repeat( "\t", ( count( $parent_stack ) + 1 ) * 2 ) . '</ul>';
 				$html[] = str_repeat( "\t", ( count( $parent_stack ) + 1 ) * 2 - 1 ) . '</li>';
@@ -47,7 +47,7 @@ class MenuBuilder
 			elseif ( !empty( $children[$option['value']['id']] ) )
 			{
 				$tab = str_repeat( "\t", ( count( $parent_stack ) + 1 ) * 2 - 1 );
-				
+
 				// HTML for menu item containing childrens (open)
 				$html[] = sprintf(
 					'%1$s<li><a class="menu" href="%2$s">%3$s</a>',
@@ -56,7 +56,7 @@ class MenuBuilder
 					$option['value']['name']   // %3$s = title
 				); 
 				$html[] = $tab . "\t" . '<ul class="menu-dropdown">';
-				
+
 				array_push( $parent_stack, $option['value']['parent_id'] );
 				$parent = $option['value']['id'];
 			}
@@ -69,10 +69,10 @@ class MenuBuilder
 					$option['value']['name']   // %3$s = title
 				);
 		}
-		
+
 		// HTML wrapper for the menu (close)
 		$html[] = "</ul>";
-		
+
 		return implode($html);
 	}
 
@@ -84,26 +84,26 @@ class MenuBuilder
 	{
 		$html  = array();
 		$items = \DB::select()->from('menus')->order_by('parent_id', 'asc')->order_by('position', 'asc')->as_assoc()->execute();
-		
+
 		foreach ( $items as $item )
 			$children[$item['parent_id']][] = $item;
-		
+
 		// loop will be false if the root has no children (i.e., an empty menu!)
 		$loop = !empty( $children[$root_id] );
-		
+
 		// initializing $parent as the root
 		$parent = $root_id;
 		$parent_stack = array();
-		
+
 		// HTML wrapper for the menu (open)
 		$html[] = "<ul class='sortable'>";
-		
+
 		while ( $loop && ( ( $option = each( $children[$parent] ) ) || ( $parent > $root_id ) ) )
 		{
 			if ( $option === false )
 			{
 				$parent = array_pop( $parent_stack );
-				
+
 				// HTML for menu item containing childrens (close)
 				$html[] = str_repeat( "\t", ( count( $parent_stack ) + 1 ) * 2 ) . '</ul>';
 				$html[] = str_repeat( "\t", ( count( $parent_stack ) + 1 ) * 2 - 1 ) . '</li>';
@@ -111,7 +111,7 @@ class MenuBuilder
 			elseif ( !empty( $children[$option['value']['id']] ) )
 			{
 				$tab = str_repeat( "\t", ( count( $parent_stack ) + 1 ) * 2 - 1 );
-				
+
 				// HTML for menu item containing childrens (open)
 				$html[] = sprintf(
 					'%1$s<li id="list_%4$s"><div><a href="%2$s" id="item_%4$s">%3$s</a><a href="" onclick="return false" id="%4$s" class="edit"> edit</a><a href="" onclick="return false" id="%4$s" class="delete"> delete</a></div>',
@@ -121,7 +121,7 @@ class MenuBuilder
 					$option['value']['id']		// %4$s = id 
 				); 
 				$html[] = $tab . "\t" . '<ul>';
-				
+
 				array_push( $parent_stack, $option['value']['parent_id'] );
 				$parent = $option['value']['id'];
 			}
@@ -135,18 +135,18 @@ class MenuBuilder
 					$option['value']['id']		// %4$s = id 
 				);
 		}
-		
+
 		// HTML wrapper for the menu (close)
 		$html[] = "</ul>";
-		
+
 		return implode($html);
 	  }
 
 
 
-	
 
-	
+
+
 
 
 
