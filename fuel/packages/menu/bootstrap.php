8655a51cf96d642b1d026284ada375b622edd16e
jamieshiers <jamie@digitalschool.co.uk>
2012-09-16 17:42:40 +0100

    Upadates to fix pagodabox deploy issues

--- fuel/packages/menu/bootstrap.php
+++ fuel/packages/menu/bootstrap.php
@@ -9,7 +9,7 @@ Autoloader::add_core_namespace('Menu');
 
 // Add as core namespace (classes are aliased to global, thus useable without namespace prefix)
 // Set the second argument to true to prefix and be able to overwrite core classes
-Autoloader::add_core_namespace('Menu', true);
+Autoloader::add_core_namespace('Menu');
 
 // And add the classes, this is useful for:
 // - optimization: no path searching is necessary
