65b1d0daa07c6c2b3c21cd522c701ed7c9922c73
schcms <schcms@pagodabox.com>
2012-09-16 11:18:03 -0500

    Deploy point 34

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host='.$_SERVER['DB1_HOST'].'port='.$_SERVER['DB1_PORT'].';dbname='.$_SERVER['DB1_NAME'],
+			'dsn'        => 'mysql:host='.$_SERVER['DB1_HOST'].';port='.$_SERVER['DB1_PORT'].';dbname='.$_SERVER['DB1_NAME'],
 			'username'   => $_SERVER['DB1_USER'],
 			'password'   => $_SERVER['DB1_PASS'],
 		),
